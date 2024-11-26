67df458f54d68190ac123ddf4d82632124af022e
smlbrd <smallbirdquickthorn+github@gmail.com>
2024-11-26 14:02:47 +0000

    refactor endpoint

--- db/app.js
+++ db/app.js
@@ -33,7 +33,6 @@ app.get('/api/articles', getArticles);
 app.get('/api/articles/:article_id', getArticleById);
 
 app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
-
 app.post('/api/articles/:article_id/comments', postCommentByArticleId);
 
 app.all('*', notFoundErrorHandler);
