015683d8a5153509520f2f5fee8690c2759cad12
smlbrd <smallbirdquickthorn+github@gmail.com>
2024-11-26 15:15:33 +0000

    Refactor checkExists into utility function

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,4 +1,5 @@
 const db = require('../../db/connection');
+const format = require('pg-format');
 
 exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   if (!created_at) return { ...otherProperties };
@@ -20,5 +21,24 @@ exports.formatComments = (comments, idLookup) => {
       author: created_by,
       ...this.convertTimestampToDate(restOfComment),
     };
+  });
+};
+
+exports.checkExists = (table, column, value) => {
+  const queryString = format(
+    `SELECT * 
+    FROM %I 
+    WHERE %I = $1`,
+    table,
+    column
+  );
+
+  return db.query(queryString, [value]).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: `Not Found`,
+      });
+    }
   });
 };
